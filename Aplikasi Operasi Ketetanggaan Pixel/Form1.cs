e9ad64fb978022b4664b7bf7ed408cefc387dc15
I Putu Agus Suarya W <agus.suarya@yahoo.co.id>
2015-11-05 15:34:37 +0800

    Form awal ne

---
+++ Aplikasi Operasi Ketetanggaan Pixel/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplikasi_Operasi_Ketetanggaan_Pixel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
